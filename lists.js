4f9d16119886b53a67c0c7a8baf57a6f039afb90
vbst7 <valerieb7000@gmail.com>
2024-05-01 14:11:15 -0400

    Update lists.js

--- lists.js
+++ lists.js
@@ -1,3 +1,25 @@
+const cardType = Object.freeze({
+    EVENT: Symbol('event'),
+    LANDMARK: Symbol('landmark'),
+    PROJECT: Symbol('project'),
+    WAY: Symbol('way'),
+    TRAIT: Symbol('trait'),
+    PROPHESY: Symbol('prophesy')
+})
+
+class cardShapedThing{
+    constructor(url, type){
+        this.URL = url
+        this.type = type
+    }
+    getURL(){
+        return URL
+    }
+    getType(){
+        return type
+    }
+}
+
 //Constant arrays containing objects of each card
 //image URL from the DominionStrategyWiki
 
